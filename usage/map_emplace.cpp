117bb7d74ef1bed43c29c3fdc2859a1e6f5320bf
zhaoxs <zhaoxs15@163.com>
2021-11-11 21:11:01 +0800

    std::map emplace

---
+++ usage/map_emplace.cpp
@@ -0,0 +1,30 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+struct DocInfo {
+  std::string a;
+  std::string b;
+};
+
+struct Elem {
+  std::string id;
+  double score;
+  DocInfo doc_info;
+  Elem(std::string a, double b) :
+      id(a),
+      score(b) {}
+  std::string to_string() {
+    return id + std::to_string(score);
+  }
+};
+
+int main(void) {
+  std::map<std::string, Elem> map;
+  map.emplace("a", Elem("aa", 0.1));
+
+  for (auto [key, value] : map) {
+    std::cout << key << " " << value.to_string() << std::endl;
+  }
+  return 0;
+}
