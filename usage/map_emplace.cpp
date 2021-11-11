12de388000e8f3828e69cbacbc9e40463a540953
zhaoxs <zhaoxs15@163.com>
2021-11-11 21:18:57 +0800

    map emplace

--- usage/map_emplace.cpp
+++ usage/map_emplace.cpp
@@ -22,6 +22,8 @@ struct Elem {
 int main(void) {
   std::map<std::string, Elem> map;
   map.emplace("a", Elem("aa", 0.1));
+  Elem elem("bb", 1.1);
+  map.emplace("b", std::move(elem));
 
   for (auto [key, value] : map) {
     std::cout << key << " " << value.to_string() << std::endl;
